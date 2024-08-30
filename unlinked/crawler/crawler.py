661b7c9b2616974a637809a6682bba73d4595998
Aashish1-1-1 <aashishadhikari693@gmail.com>
2024-08-30 17:09:00 +0545

    [Format]

---
+++ unlinked/crawler/crawler.py
@@ -0,0 +1,14 @@
+class crawler:
+    def __init__(self,startlink:str):
+        self.startlink=startlink
+        self.crawstack=[]
+        self.contexttext=[]
+    def parsedom(self):
+    #Fetch html of the link provided and parse to get links and tags befor that anchor tag
+
+    def extracttext():
+    #Extract the text and link out of the div or paragraph in which anchor tag is in and save it as list in out crawstack
+
+    def fetchlink(self):
+    #Iterate through stack and fetch that html and contextual text with which a page can be judged save them in contexttext list as a list of list and at last return it 
+    
---
+++ unlinked/transformer/transformer.py
@@ -0,0 +1,11 @@
+class transformer:
+    def __init__(self):
+        
+    def Embedd(self):
+    #Call fetchlink from crawler which will return list of list of text and embedded them
+
+    def Attention(self):
+    #Make sense out of sentence by keeping them in attention layer where they make sense to each other useful for some word with double meaning
+
+    def Compare(self):
+    #Compare the cosine of two textual from different site and calculate the average and probability of them being related now return probability and correspoding link
